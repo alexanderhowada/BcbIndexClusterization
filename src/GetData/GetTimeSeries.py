d2c77190d92b157298d265398b7d340938b68fd4
ahow <alexanderhidekioniwa@gmail.com>
2022-08-08 02:33:35 -0300

    modified to work as iterator and removed tqdm

--- src/GetData/GetTimeSeries.py
+++ src/GetData/GetTimeSeries.py
@@ -1,6 +1,6 @@
 import sgs
 import pandas as pd
-from tqdm import tqdm
+#from tqdm import tqdm
 
 
 class GetTimeSeries:
@@ -12,8 +12,10 @@ class GetTimeSeries:
 		GTS = GetTimeSeries()
 		df, meta = GTS([1, 188], start_date='20/12/2017', end_date='31/03/2018')
 	"""
-	def __init__(self):
-		pass
+	def __init__(self, ids=None, start_date=None, end_date=None):
+		self.ids = ids
+		self.start_date = start_date
+		self.end_date = end_date
 
 	def __call__(self, ids, start_date, end_date):
 		"""
@@ -26,14 +28,36 @@ class GetTimeSeries:
 
 		df = self._get_raw_dataframe(ids, start_date, end_date)
 		meta = self._get_metadata(df)
-		df = self._assign_columns(df, meta)
+		# df = self._assign_columns(df, meta)
 
 		return df, meta
+
+	def __iter__(self):
+		self.i = 0
+		return self
+
+	def __len__(self):
+		if self.ids is None:
+			return 0
+		else:
+			return len(self.ids)
+
+	def __next__(self):
+		if self.i < len(self):
+			df = self._get_raw_dataframe(
+				[self.ids[self.i]], self.start_date, self.end_date
+			)
+			meta = self._get_metadata(df)
+
+			self.i += 1
+			return df, meta
+		else:
+			return None, None
 
 	@staticmethod
 	def _get_raw_dataframe(series_ids, start_date, end_date):
 		full = []
-		for idx in tqdm(series_ids):
+		for idx in series_ids:
 			ts = sgs.time_serie(idx, start=start_date, end=end_date)
 			full.append(ts)
 		full = pd.concat(full, axis=1)
